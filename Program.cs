c85ba1e1e73cf333cb294982b2634cae1366c06d
Joanderson SIlva <jhonny_040996@hotmail.com>
2021-11-22 12:28:53 -0300

    Projeto Csharp

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace _1_PrimeiroProjeto
+{
+    class Programa
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, mundo. Projeto no Visual Studio!");
+
+            Console.WriteLine("A execução terminou. Tecle enter para fechar...");
+            Console.ReadLine(); 
+        }
+    }
+}
